2a37a3eaa7287e000dc68f4ae272a9def6ef4a51
Deivard Johannes (Consultant) <johannes.deivard@consultant.volvo.com>
2022-12-01 09:53:23 +0100

    Part 2 solution added

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -17,17 +17,31 @@ def get_highest_calory_inventory(inventories):
     return index, sum(inventory)
 
 
-def main():
+def get_n_highest_calory_inventories(inventories, n):
+    inventories_sorted = sorted(map(sum, inventories))
+    return inventories_sorted[-n:]
+
+
+def part_1():
     inventories = get_inventories_from_file("day_01/input.txt")
     index, highest_calory_inventory = get_highest_calory_inventory(inventories)
     print(f"Highest calory invetory belongs to elf number {index}, with {highest_calory_inventory} calories in it.")
 
 
+def part_2():
+    inventories = get_inventories_from_file("day_01/input.txt")
+    highest_calory_inventories = get_n_highest_calory_inventories(inventories, 3)
+    print(sum(highest_calory_inventories))
+
+
 if __name__=="__main__":
-    main()
-    # One liner/code golf
+    part_1()
+    # Part 1 - One liner/code golf
     print(max([sum(map(int,i.strip().split("\n")))for i in open("day_01/input.txt").read().split("\n\n")]))
     
+    part_2()
+    # Part 2 - One liner/code golf
+    print(sum(sorted([sum(map(int,i.strip().split("\n")))for i in open("day_01/input.txt").read().split("\n\n")])[-3:]))
 
     
         
