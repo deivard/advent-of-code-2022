6c0f17a4095696dfd5e41cd96c6185e24b1baaa9
Deivard Johannes (Consultant) <johannes.deivard@consultant.volvo.com>
2022-12-02 10:32:23 +0100

    🎄

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -13,7 +13,10 @@ def get_inventories_from_file(filename):
 
 
 def get_highest_calory_inventory(inventories):
-    index, inventory = max(enumerate(inventories), key=lambda enumeration: sum(enumeration[1]))
+    index, inventory = max(
+        enumerate(inventories),
+        key=lambda enumeration: sum(enumeration[1])
+    )
     return index, sum(inventory)
 
 
@@ -25,23 +28,23 @@ def get_n_highest_calory_inventories(inventories, n):
 def part_1():
     inventories = get_inventories_from_file("day_01/input.txt")
     index, highest_calory_inventory = get_highest_calory_inventory(inventories)
-    print(f"Highest calory invetory belongs to elf number {index}, with {highest_calory_inventory} calories in it.")
+    print(f"Highest calory invetory belongs to elf number {index}, with "
+          f"{highest_calory_inventory} calories in it.")
 
 
 def part_2():
     inventories = get_inventories_from_file("day_01/input.txt")
-    highest_calory_inventories = get_n_highest_calory_inventories(inventories, 3)
+    highest_calory_inventories = get_n_highest_calory_inventories(
+        inventories, 3
+    )
     print(sum(highest_calory_inventories))
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     part_1()
     # Part 1 - One liner/code golf
     print(max([sum(map(int,i.strip().split("\n")))for i in open("day_01/input.txt").read().split("\n\n")]))
-    
+
     part_2()
     # Part 2 - One liner/code golf
     print(sum(sorted([sum(map(int,i.strip().split("\n")))for i in open("day_01/input.txt").read().split("\n\n")])[-3:]))
-
-    
-        
