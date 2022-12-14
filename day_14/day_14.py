0ca6feb348eb2e7181d4d492644384ba8c4830c0
deivard <jdeivard@outlook.com>
2022-12-14 18:57:33 +0100

    ✨

--- day_14/day_14.py
+++ day_14/day_14.py
@@ -2,8 +2,8 @@
 """
 
 
-import sys
-# import resource
+import os
+import time
 
 
 def get_rock_paths_from_lines(lines: list[str]):
@@ -17,20 +17,6 @@ def get_rock_paths_from_lines(lines: list[str]):
 
 def find_min_max(iterable):
     return min(iterable), max(iterable)
-
-
-# def find_path_y_min_max(rock_paths):
-#     return 
-    
-#     y_min = rock_paths[0][0][1]
-#     y_max = y_min
-#     for path in rock_paths:
-#         for point in path:
-#             if point[1] > y_max:
-#                 y_max = point[0]
-#             if point[1] > y_max:
-#                 y_max = point[1]
-#     return y_min, y_max
 
 
 def find_map_boundaries(rock_paths: list[tuple[int, int]]):
@@ -61,8 +47,8 @@ def find_sand_boundaries(cave_map):
             (min(x_max+1, len(cave_map[0])), min(y_max+1, len(cave_map))))
 
 
-def create_empty_map_from_boundaries(max_boundary: tuple[int, int]):
-    return [["." for _ in range(max_boundary[0]+1)] 
+def create_empty_map_from_max_boundary(max_boundary: tuple[int, int]):
+    return [["." for _ in range(max_boundary[0]+1)]
             for _ in range(max_boundary[1]+1)]
 
 
@@ -71,7 +57,7 @@ def auto_crop_map(cave_map, boundaries):
     x_max = boundaries[1][0]+1
     y_min = boundaries[0][1]
     y_max = boundaries[1][1]+1
-    
+
     cropped_map = []
     for row in cave_map[y_min:y_max]:
         cropped_map.append(row[x_min:x_max])
@@ -85,10 +71,10 @@ def place_rock_paths_on_map(paths: list[list[tuple[int, int]]],
         for i in range(len(path)-1):
             x_start = min((path[i+1][0], path[i][0]))-1
             x_end = max((path[i+1][0], path[i][0]))
-            
+
             y_start = min((path[i+1][1], path[i][1]))-1
             y_end = max((path[i+1][1], path[i][1]))
-            
+
             for x in range(x_start, x_end):
                 for y in range(y_start, y_end):
                     cave_map[y+1][x+1] = "#"
@@ -108,21 +94,28 @@ def print_map(cave_map: list[list[str]], boundaries=None):
         y_min = 0
         y_max = len(cave_map)-1
 
+    map_str = ""
+    line_rows = 2
+    print()  # Empty row
     print(f"{' '.rjust(3)} "
           + str(x_min) + " "
           + str(x_max).rjust((x_max-x_min) - len(str(x_max))))
     for y in range(y_min, y_max+1):
-        print(f"{str(y).rjust(3)} " + "".join(cave_map[y][x_min:x_max+1])
-              .replace("o", "\033[0;93mo\033[0m")
-              .replace("#", "\033[0;30m#\033[0m")
-              .replace(".", "\033[0;94m.\033[0m"))
+        map_str += (f"{str(y).rjust(3)} " + "".join(cave_map[y][x_min:x_max+1])
+                    .replace("o", "\033[0;93mo\033[0m")
+                    .replace("#", "\033[0;30m#\033[0m")
+                    .replace(".", "\033[0;94m.\033[0m")
+                    + "\n")
+        line_rows += 1
+    print(map_str)
+    return line_rows
 
 
 def find_sand_landing_position(cave_map, sand_index=(0, 500)):
     max_row = len(cave_map)-1
     y, x = sand_index
     if y+1 > max_row or (0 > x >= len(cave_map[0])):
-        raise Exception("Sand flowed to the abyss")
+        raise ValueError("Sand flowed to the abyss")
     elif cave_map[y+1][x] == ".":
         position = find_sand_landing_position(cave_map, (y+1, x))
     else:
@@ -167,43 +160,85 @@ def find_sand_landing_position_infinite_floor(cave_map,
     if x >= map_width-2 or x < 1:
         cave_map = increase_map_width(cave_map, map_width_increment)
         x_offset += map_width_increment
-        position, x_offset = find_sand_landing_position_infinite_floor(cave_map, x_offset, (y, x + map_width_increment))
+        x += map_width_increment
 
     if cave_map[y+1][x] == ".":
         collision_point = find_point_before_collision((y, x), cave_map)
-        position, x_offset = find_sand_landing_position_infinite_floor(cave_map, x_offset, collision_point)
+        position, x_offset = find_sand_landing_position_infinite_floor(
+            cave_map, x_offset, collision_point
+        )
     else:
         # Left down
-        if cave_map[y+1][x-1] == ".":# and cave_map[y][x-1] == ".":
-            position, x_offset = find_sand_landing_position_infinite_floor(cave_map, x_offset, (y+1, x-1))
+        if cave_map[y+1][x-1] == ".":
+            position, x_offset = find_sand_landing_position_infinite_floor(
+                cave_map, x_offset, (y+1, x-1)
+            )
         # Right down
-        elif cave_map[y+1][x+1] == ".":#  and cave_map[y][x+1] == ".":
-            position, x_offset = find_sand_landing_position_infinite_floor(cave_map, x_offset, (y+1, x+1))
+        elif cave_map[y+1][x+1] == ".":
+            position, x_offset = find_sand_landing_position_infinite_floor(
+                cave_map, x_offset, (y+1, x+1)
+            )
         # Settled
         else:
             position = (y, x)
-        
+
     return position, x_offset
-                    
+
+
+def visualization_demo():
+    os.system("cls")
+    input("Maximize your terminal and press enter to start...")
+
+    lines = open("day_14/demo_input.txt").readlines()
+    rock_paths = get_rock_paths_from_lines(lines)
+    _, max_boundaries = find_map_boundaries(rock_paths)
+    max_boundaries = max_boundaries[0], max_boundaries[1] + 2
+    cave_map = create_empty_map_from_max_boundary(max_boundaries)
+    cave_map = place_rock_paths_on_map(rock_paths, cave_map)
+    # Remove part of the map so it fits the terminal
+    cave_map = cave_map[5:5+os.get_terminal_size().lines-4]
+    cave_map[-1] = ["#" for _ in range(len(cave_map[-1]))]
+
+    sand_start_pos = (0, 500)
+    start_offset = 0
+    num_sand_settled = 0
+    printed_rows = 0
+    sleep_time = 0.1
+    min_sleep_time = 0
+    while cave_map[sand_start_pos[0]][sand_start_pos[1]] != "o":
+        sand_start_pos = (0, 500+start_offset)
+        sand_pos, start_offset = find_sand_landing_position_infinite_floor(
+            cave_map, start_offset, sand_index=sand_start_pos
+        )
+        cave_map[sand_pos[0]][sand_pos[1]] = "o"
+        num_sand_settled += 1
+
+        sand_boundaries = find_sand_boundaries(cave_map)
+        printed_rows = print_map(cave_map, sand_boundaries)
+        print("\033[F"*(printed_rows+2), end="")
+        time.sleep(sleep_time)
+        sleep_time = max(sleep_time*0.99, min_sleep_time)
+
+    sand_boundaries = find_sand_boundaries(cave_map)
+    print_map(cave_map, sand_boundaries)
+    print(f"Part 2 - Sand reached the top after {num_sand_settled}"
+          " sand units had settled.")
 
 
 def part_1():
     lines = open("day_14/input.txt").readlines()
     rock_paths = get_rock_paths_from_lines(lines)
     min_boundaries, max_boundaries = find_map_boundaries(rock_paths)
-    cave_map = create_empty_map_from_boundaries(max_boundaries)
+    cave_map = create_empty_map_from_max_boundary(max_boundaries)
     cave_map = place_rock_paths_on_map(rock_paths, cave_map)
-    
-    # print_map(cave_map, ((min_boundaries[0], 0), max_boundaries))
+
     num_sand_settled = 0
     while True:
         try:
             sand_position = find_sand_landing_position(cave_map, (0, 500))
             cave_map[sand_position[0]][sand_position[1]] = "o"
             num_sand_settled += 1
-            # print_map(cave_map, ((min_boundaries[0], 0), max_boundaries))
-        except Exception as e:
-            # print(e)
+        except ValueError:
             break
 
     print_map(cave_map, ((min_boundaries[0], 0), max_boundaries))
@@ -211,34 +246,25 @@ def part_1():
           " sand units had settled.")
 
 
-def count_sand(cave_map):
-    pass
-
 def part_2():
-    sys.setrecursionlimit(500000)
-
     lines = open("day_14/input2.txt").readlines()
     rock_paths = get_rock_paths_from_lines(lines)
-    min_boundaries, max_boundaries = find_map_boundaries(rock_paths)
+    _, max_boundaries = find_map_boundaries(rock_paths)
     max_boundaries = max_boundaries[0], max_boundaries[1] + 2
-    cave_map = create_empty_map_from_boundaries(max_boundaries)
+    cave_map = create_empty_map_from_max_boundary(max_boundaries)
     cave_map = place_rock_paths_on_map(rock_paths, cave_map)
     cave_map[-1] = ["#" for _ in range(len(cave_map[-1]))]
-    
+
     sand_start_position = (0, 500)
     start_offset = 0
-    print_map(cave_map, ((min_boundaries[0], 0), max_boundaries))
     num_sand_settled = 0
     while cave_map[sand_start_position[0]][sand_start_position[1]] != "o":
         sand_start_position = (0, 500+start_offset)
-        sand_position, start_offset = find_sand_landing_position_infinite_floor(
+        sand_pos, start_offset = find_sand_landing_position_infinite_floor(
             cave_map, start_offset, sand_index=sand_start_position
         )
-        cave_map[sand_position[0]][sand_position[1]] = "o"
+        cave_map[sand_pos[0]][sand_pos[1]] = "o"
         num_sand_settled += 1
-        # print(num_sand_settled)
-        # sand_boundaries = find_sand_boundaries(cave_map)
-        # print_map(cave_map, sand_boundaries)
 
     sand_boundaries = find_sand_boundaries(cave_map)
     print_map(cave_map, sand_boundaries)
@@ -247,8 +273,9 @@ def part_2():
 
 
 def main():
-    # part_1()
+    part_1()
     part_2()
+    visualization_demo()
 
 
 if __name__ == "__main__":
